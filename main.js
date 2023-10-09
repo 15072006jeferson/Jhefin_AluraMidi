9b23e82b57d10ca7b8be864fef9ba154b3569570
15072006jeferson <141078597+15072006jeferson@users.noreply.github.com>
2023-10-09 19:43:38 -0300

    ok

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 
-While(contador <9 )[
+While(contador <9 ){
     ListaDeTeclas[0].onclick = tocaSomPom;
 
 contador = contador + 1;
 
 console.log(contador);
-]
+}
 
 
 
