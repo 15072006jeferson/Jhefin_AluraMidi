48a3bc83d8781295ba3bbc187df5e984425bdd4a
15072006jeferson <141078597+15072006jeferson@users.noreply.github.com>
2023-10-16 19:59:45 -0300

    ok

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// Pom
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();  
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();  
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
@@ -9,7 +8,7 @@ let contador = 0;
 
 //enquanto
 while (contador < ListaDeTeclas.length){
-    ListaDeTeclas[contador].onclick = tocaSomPom;
+    ListaDeTeclas[contador].onclick = tocaSom;
 
     contador = contador + 1;
 
